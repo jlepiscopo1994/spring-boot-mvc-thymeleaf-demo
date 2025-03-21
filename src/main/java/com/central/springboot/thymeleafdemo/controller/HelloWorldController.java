6d143624d4d65fec8b41603f847d008259ac1ee7
Joseph Lepiscopo <joeycentral@gmail.com>
2025-03-21 15:44:22 -0400

    Added new post mapping to controller and modified views to manipulate request model to manipulate it in a simple way.

--- src/main/java/com/central/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/central/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.central.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,25 @@ public class HelloWorldController {
     // Process HTML Form
     @PostMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    // Add controller method to read form data and add data to model
+
+    @PostMapping("/processFormV2")
+    public String allCaps(HttpServletRequest request, Model model) {
+
+        // Read request parameter from form
+        String name = request.getParameter("studentName");
+
+        // convert message to all caps and concap message
+        name = name.toUpperCase();
+
+        String result = "STOP SHOUTING " + name + "!";
+
+        // add message to model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
