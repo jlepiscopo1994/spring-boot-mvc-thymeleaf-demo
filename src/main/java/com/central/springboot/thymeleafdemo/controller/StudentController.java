dad202222ccd639e7f777d38da5fdae25eff2a8d
Joseph Lepiscopo <joeycentral@gmail.com>
2025-03-24 10:33:28 -0400

    Added a Student model, controller, and two forms for collecting and processing student data.

---
+++ src/main/java/com/central/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.central.springboot.thymeleafdemo.controller;
+
+import com.central.springboot.thymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model model) {
+
+        Student student = new Student();
+
+        model.addAttribute("student", student);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student student) {
+
+        System.out.println("Student: " + student.getFirstName() + " " + student.getLastName());
+
+        return "student-confirmation";
+    }
+}
