0a47edce86ec10a585b5deafc6111752e074aadf
Joseph Lepiscopo <joeycentral@gmail.com>
2025-03-24 11:46:05 -0400

    Modified previous commit to eliminate hard coded checkboxes and read list of checkboxes from applications.properties file.

--- src/main/java/com/central/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/central/springboot/thymeleafdemo/controller/StudentController.java
@@ -18,6 +18,9 @@ public class StudentController {
     @Value("${languages}")
     private List<String> languages;
 
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
 
@@ -28,6 +31,8 @@ public class StudentController {
         model.addAttribute("countries", countries);
 
         model.addAttribute("languages", languages);
+
+        model.addAttribute("systems", systems);
 
         return "student-form";
     }
