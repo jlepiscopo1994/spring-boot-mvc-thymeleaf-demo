5f8dca241be7fd58564e8a89c2b29e35bd85426d
Joseph Lepiscopo <joeycentral@gmail.com>
2025-03-24 11:14:15 -0400

    Modified the drop-down implementation to read options from applications.properties.

--- src/main/java/com/central/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/central/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,8 +1,10 @@
 package com.central.springboot.thymeleafdemo.controller;
 
 import com.central.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +12,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class StudentController {
 
+    @Value("${countries}")
+    private List<String> countries;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
 
         Student student = new Student();
 
         model.addAttribute("student", student);
+
+        model.addAttribute("countries", countries);
 
         return "student-form";
     }
